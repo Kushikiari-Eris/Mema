3285686eb3dd411e85ae22e73fdd5debf4b34bc3
kushikiari-Eris <erispena303@gmail.com>
2024-09-08 20:00:36 +0800

    awdawd

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import Dashboard from './pages/Dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-       <App />
+       <Dashboard />
     </BrowserRouter>
   </StrictMode>,
 )
